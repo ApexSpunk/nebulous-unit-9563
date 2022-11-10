4c11940c77caf64e0d0b934501ea7c7e02999446
aakash <37771235+aakashindoriya@users.noreply.github.com>
2022-11-10 22:31:17 +0530

    home page done

--- src/Components/homepage/homePageComCarosel.jsx
+++ src/Components/homepage/homePageComCarosel.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import { FiChevronLeft,FiChevronRight} from "react-icons/fi";
 import img1 from "../homepagePhotos/img1.jpg"
 import img2 from "../homepagePhotos/img2.jpg"
@@ -13,6 +13,7 @@ import kid2 from "../homepagePhotos/kids2.jpg"
 import kid3 from "../homepagePhotos/kids3.jpg"
 import kidvid from "../homepagePhotos/kidvid.mp4"
 import HomePageCarosel from "./HomePageSlider";
+import {motion} from "framer-motion"
 import { useState } from "react";
 export default function HomePageComCarosel(){
     const woman=[{item:img1,isvedio:false},{item:img2,isvedio:false},{item:img3,isvedio:false},{item:img4,isvedio:false},{item:img5,isvedio:false}];
@@ -24,19 +25,24 @@ export default function HomePageComCarosel(){
         <Box w="100%"  h={"100vh"} >
             
             <Flex alignItems={"center"}  >
-                <Box pos={"absolute"} left={10} visibility={count<=1?"hidden":"visible"} zIndex="10" ><FiChevronLeft size={50} onClick={()=>{
+                <Box alignItems={"center"}
+                as={motion.div} whileHover={{scale:1.3,cursor:"pointer",  } } transition={{duration:0.8}}
+                pos={"absolute"} left={10} visibility={count<=1?"hidden":"visible"} zIndex="10" ><Flex alignItems={"center"}><FiChevronLeft size={40} onClick={()=>{
                     setCount(count-1);
                     setIsleft(true)
-                }}/></Box>
+                }}/> <Text fontSize={9} ml={-3}>{count===2?"WOMAN":"MAN"}</Text></Flex></Box>
                 <Box  w="100%"  h={"100vh"} >
             {count===1&&<HomePageCarosel images={woman} isleft={isleft} />}
             {count===2&&<HomePageCarosel images={man} isleft={isleft}/>}
             {count===3&&<HomePageCarosel images={kids} isleft={isleft}/>}
                 </Box >
-                <Box pos={"absolute"} right={10} visibility={count>=3?"hidden":"visible"} ><FiChevronRight size={50} onClick={()=>{
+                <Box 
+                as={motion.div} whileHover={{scale:1.3,cursor:"pointer",  } } transition={{duration:0.8}}
+                pos={"absolute"} right={10} visibility={count>=3?"hidden":"visible"} ><Flex alignItems={"center"}><Text fontSize={9} mr={-3}>{count===2?"KIDS":"MAN"}</Text>
+                <FiChevronRight size={40} onClick={()=>{
                     setCount(count+1);
                     setIsleft(false)
-                }}/></Box>
+                }}/></Flex></Box>
             </Flex>
         
             
--- src/Components/homepage/pagesLinks.tsx
+++ src/Components/homepage/pagesLinks.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function PageLinks(){
     return (
-        <Box w="100%" pos={"absolute"}  zIndex={1000} bottom={"20%"}>
+        <Box w="100%" pos={"absolute"}  zIndex={1000} bottom={"8%"}>
                             <Flex w="60%" justify={"space-around"} h="70px" m="auto">
-                            <Link to="/" ><Text as={"b"} fontSize={32}>new</Text> </Link>
-                            <Link to="/" ><Text fontSize={32}>new</Text></Link>
-                            <Link to="/" ><Text fontSize={32}>new</Text></Link>
+                            <Link to="/" ><Text as={"b"} fontSize={23}>new</Text> </Link>
+                            <Link to="/" ><Text fontSize={23}>new</Text></Link>
+                            <Link to="/" ><Text fontSize={23}>new</Text></Link>
                             </Flex>
                         </Box>
     )
