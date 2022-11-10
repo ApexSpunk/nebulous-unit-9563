a440cd7623c21662b0936caec637c156ca31cca8
aakash <37771235+aakashindoriya@users.noreply.github.com>
2022-11-10 15:01:17 +0530

    home page almost done

--- src/Components/homepage/HomePageSlider.jsx
+++ src/Components/homepage/HomePageSlider.jsx
@@ -1,11 +1,14 @@
-import {Box} from '@chakra-ui/react'
+import {Box, Flex, Heading, Text} from '@chakra-ui/react'
 
 import {useState} from "react"
 import {motion} from "framer-motion"
+import { Link } from 'react-router-dom'
+import Footer from './fotter'
+import PageLinks from './pagesLinks'
 
 
 
-export default function HomePageCarosel({images}){
+export default function HomePageCarosel({images,isleft,links}){
     let [cindex,setCindex]=useState(0)
     let [isup,setIsup]=useState(false)
   
@@ -47,13 +50,15 @@ export default function HomePageCarosel({images}){
        
     }
     return (
-        <Box   >
-            <Box w="100%"  h={"100vh"} border={"6px solid"} overflow="hidden" onWheel={(e)=>{e.deltaY>0?handleNext():handlePrev()}} >
+        <motion.div initial={{opacity:0,x:isleft?"-100vh":"100vh"}}
+        animate={{opacity:1,x:0,}}
+        transition={{type:"tween", duration:0.9}}>
+            <Box w="100%"  h={"100vh"} overflow="hidden" onWheel={(e)=>{e.deltaY>0?handleNext():handlePrev()}} >
                 
                 {images.slice(cindex,cindex+1).map((el)=>{
                     return(
 
-                        <Box key={el.item}>{!el.isvedio?<motion.img  
+                        <Box key={el.item} pos="relative">{!el.isvedio?<motion.img  
                         initial={{opacity:0,y:isup?"100vh":"-100vh"}}
                         animate={{opacity:1,y:0}}
                         transition={{type:"tween", duration:0.5}}
@@ -66,12 +71,15 @@ export default function HomePageCarosel({images}){
                         <source   src={el.item} type="video/mp4"/>
                         </motion.video>
                         
-                        }</Box>
+                        }
+                        {cindex!==images.length-1&&<PageLinks />}
+                     {  cindex===images.length-1&&<Footer />}
+                        </Box>
                     )
                     
                     })}
             </Box>
 
-        </Box>
+            </motion.div>
     )
 }
