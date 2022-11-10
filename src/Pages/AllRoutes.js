e513a17f5f59d9ea5a88acbb11f9eb1ed013c6c1
Abhra11 <abhra001597@gmail.com>
2022-11-10 23:48:46 +0530

    in progress

--- src/Pages/AllRoutes.js
+++ src/Pages/AllRoutes.js
@@ -1,31 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../Components/Admin/Dashboard';
-import Products from '../Components/Admin/Products';
-import Admin from './Admin';
-import Home from './Home';
-import Womens from './Womens';
-import Mens from './Mens';
-import Childs from './Childs';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Components/Admin/Dashboard";
+import Products from "../Components/Admin/Products";
+import Admin from "./Admin";
+import Home from "./Home";
+import Womens from "./Womens";
+import Mens from "./Mens";
+import Childs from "./Childs";
 import Search from "./Search";
-
+import SearchMen from "./SearchMen";
+import SearchWoman from "./SearchWoman";
 
 const AllRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/womens" element={<Womens />} />
-            <Route path="/mens" element={<Mens />} />
-            <Route path="/childs" element={<Childs />} />
-            <Route path="/admin" element={<Admin><Dashboard /></Admin>} />
-            <Route path="/admin/products" element={<Admin><Products /></Admin>} />
-            <Route path="*" element={<h2>404 Not Found</h2>} />
-        </Routes>
-        
-    );
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/womens" element={<Womens />} />
+      <Route path="/mens" element={<Mens />} />
+      <Route path="/childs" element={<Childs />} />
+      <Route path="searchman" element={<SearchMen />} />
+      <Route path="searchwoman" element={<SearchWoman />} />
+      <Route
+        path="/admin"
+        element={
+          <Admin>
+            <Dashboard />
+          </Admin>
+        }
+      />
+      <Route
+        path="/admin/products"
+        element={
+          <Admin>
+            <Products />
+          </Admin>
+        }
+      />
+      <Route path="*" element={<h2>404 Not Found</h2>} />
+    </Routes>
+  );
+};
 
 export default AllRoutes;
-
-
-
---
+++ src/Pages/SearchWoman.jsx
@@ -0,0 +1,51 @@
+import React,{useEffect} from 'react';
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from 'react-router-dom';
+import { getProducts } from '../Redux/products/actions'
+import "./Search.css"
+
+const SearchWoman = () => {
+    const dispatch=useDispatch();
+    const {loading,data,error}=useSelector(state=>state.product);
+    const navigate=useNavigate();
+  
+    useEffect(()=>{
+      dispatch(getProducts("Womens"))
+    },[])
+  
+    const handleMan=()=>{
+      navigate("/searchman")
+    }
+  
+    const handleWoman=()=>{
+      navigate("/searchwoman")
+    }
+  return (
+    <div>
+        <div className='section'>
+        
+        <p onClick={handleWoman}>WOMAN</p>
+        <p onClick={handleMan}>MAN</p>
+        <p>KIDS</p>
+        </div>
+        <div>
+            <input type="text" placeholder='ENTER SEARCH TERMS' className='search'  />
+        </div>
+        <div className='container2'>
+            {loading?"loading": data.map(ele=>(
+                  <div key={ele._id} className='products'>
+                  <img src={ele.images[0]} alt="" />
+                    
+                  <div style={{"display":"flex", "justifyContent":"space-between"}}>
+                  <p>{ele.title}</p>
+                  <p>₹{ele.price}</p>
+                  </div>
+                  
+                </div>
+                ))}
+          </div>
+    </div>
+  )
+}
+
+export default SearchWoman
