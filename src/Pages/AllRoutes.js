9523d64f16ac13c27b522b28e84d73dfe15edd5f
Chandan <indspunk@gmail.com>
2022-11-10 10:15:03 +0530

    Updated Routes

--- src/Pages/AllRoutes.js
+++ src/Pages/AllRoutes.js
@@ -1,21 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../Components/Admin/Dashboard';
-import Products from '../Components/Admin/Products';
-import Admin from './Admin';
-import Home from './Home';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 import Search from "./Search";
 
-
 const AllRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/admin" element={<Admin><Dashboard /></Admin>} />
-            <Route path="/admin/products" element={<Admin><Products /></Admin>} />
-            <Route path="*" element={<h2>404 Not Found</h2>} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
+  );
+};
 
 export default AllRoutes;
