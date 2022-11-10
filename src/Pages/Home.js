5fe0eea15f1587a3193a273fc600f0ed3e1d86c2
Chandan <indspunk@gmail.com>
2022-11-11 01:56:23 +0530

    Conflict Resolved

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 import HomePageComCarosel from '../Components/homepage/homePageComCarosel'
-import HomePage from '../Components/homepage/HomePageSlider'
-import { getProducts } from '../Redux/products/actions'
 
 function Home() {
-
-
   return (
-    <div>
-      <HomePageComCarosel />
-    </div>
+    <>
+     <HomePageComCarosel/>
+    </>
   )
 }
 
