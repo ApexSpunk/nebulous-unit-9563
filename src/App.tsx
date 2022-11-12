b9baedc089e7c20cc57945da93ad200719f1264d
Pushkaraj Lolusare <pushkarajlolusare24@gmail.com>
2022-11-12 23:28:52 +0530

    day 5

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './Pages/AllRoutes';
 import Register from './Components/loginLogout/Register';
+import UserLoginComponent from './Components/loginLogout/UserLoginComponent';
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <>
       {/* <AllRoutes /> */}
       <Register/>
+      {/* <UserLoginComponent/> */}
     </>
   );
 }
