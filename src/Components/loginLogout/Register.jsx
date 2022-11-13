147c8625c900ac25b774f5340002d647a406954d
Pushkaraj Lolusare <pushkarajlolusare24@gmail.com>
2022-11-13 14:02:18 +0530

    ok

--- src/Components/loginLogout/Register.jsx
+++ src/Components/loginLogout/Register.jsx
@@ -1,82 +1,95 @@
+import { useToast } from '@chakra-ui/react'
 import axios from 'axios'
-import React, { useState } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { authRegister } from '../../Redux/auth/actions'
 import "./register.css"
 
 const Register = () => {
     const [data, setData] = useState({
-        email:"",
-        password:"",
-        resetPassword:"",
-        name:"",
-        pinCode:"",
-        address:"",
-        optional:"",
-        locality:"",
-        city:"",
-        state:"",
-        phoneNumber:"",
-        news:false,
-        termCondition:false
+        email: "",
+        password: "",
+        name: "",
+        address: "",
+        locality: "",
+        city: "",
+        state: "",
+        pincode: "",
+        phone: "",
     })
-    const navigate = useNavigate()
-    const handelInput = (e)=>{
-        const {name, value, type, checked}  = e.target
-        const newValue = type==="checkbox"?checked:value
-        setData({...data,[name]:newValue})
+    const handleInput = (e) => {
+        const { name, value, type, checked } = e.target
+        const newValue = type === "checkbox" ? checked : value
+        setData({ ...data, [name]: newValue })
     }
 
-    const handelApi  = ()=>{
-        axios.post("http://localhost:8080/UserINFO",data).then(res=>{
-            console.log(res.data);
-            navigate("/login")
-        }).catch(err=>{
-            console.log(err);
-        })
-    }
+    const dispatch = useDispatch();
+    const toast = useToast();
+    const navigate = useNavigate();
 
-    const handelCreateAcc = ()=>{
-        handelApi()
-        alert("User Created!!!")
-    }
+    const { userRegister: { loading, error, message }, data: { isAuthenticated, token, user } } = useSelector(state => state.auth);
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            toast({
+                title: `Welcome ${user.name}`,
+                description: "You are logged in",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+            });
+            let time = setTimeout(() => {
+                navigate("/");
+            }, 3000);
+            return () => clearTimeout(time);
+        }
+        if (error) {
+            toast({
+                title: message,
+                description: 'Please try again',
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            });
+        }
+    }, [isAuthenticated, error]);
 
     return (
-        <>
-        <div className='main'>
+        <div className='mains'>
             <div className='detail' >
                 <h2>PERSONAL DETAILS</h2>
             </div>
             <div className='radio'>
-                <div className='insideRadio'>
-                    <input style={{ fontSize:"10px"}} type="radio" name="" id=""  onChange = {handelInput} />
-                    <label style={{marginLeft:"10px",fontSize:"10px"}} htmlFor="">PERSONAL</label>
+                <div>
+                    <input style={{ fontSize: "10px" }} checked type="radio" name="" id="" />
+                    <label style={{ marginLeft: "10px", fontSize: "10px" }} htmlFor="">PERSONAL</label>
                 </div>
-                <div className='insideRadio'>
-                    <input style={{marginLeft:"20px", fontSize:"10px"}} type="radio" name="" id=""  onChange = {handelInput} />
-                    <label style={{marginLeft:"10px",fontSize:"10px"}} htmlFor="">COMPANY</label>
+                <div>
+                    <input style={{ marginLeft: "20px", fontSize: "10px" }} type="radio" name="" id="" />
+                    <label style={{ marginLeft: "10px", fontSize: "10px", marginTop: '10px' }} htmlFor="">COMPANY</label>
                 </div>
             </div>
             <div>
-                <input style={{marginTop:'3em'}} className='inp' type="text" name='email' placeholder='E-MAIL' value={data.email} onChange = {handelInput} />
+                <input style={{ marginTop: '5em' }} className='inp' type="text" name='email' placeholder='E-MAIL' value={data.email} onChange={handleInput} />
             </div>
             <div>
-                <input style={{marginRight:"3em"}} className='inp' type="text" name='password' placeholder='PASSWORD' value={data.password} onChange = {handelInput} />
-                <input className='inp' type="text" name='resetPassword' placeholder='REPEAT PASSWORD' value={data.resetPassword} onChange = {handelInput} />
+                <input style={{ marginRight: "3em" }} className='inp' type="text" name='password' placeholder='PASSWORD' value={data.password} onChange={handleInput} />
+                <input className='inp' type="text" name='resetPassword' placeholder='REPEAT PASSWORD' value={data.resetPassword} onChange={handleInput} />
             </div>
             <div>
-                <input style={{marginRight:"3em"}} className='inp' type="text" name='name' placeholder='NAME' value={data.name} onChange = {handelInput} />
-                <input className='inp' type="text" name='pinCode' placeholder='PINCODE' value={data.pinCode} onChange = {handelInput} />
+                <input style={{ marginRight: "3em" }} className='inp' type="text" name='name' placeholder='NAME' value={data.name} onChange={handleInput} />
+                <input className='inp' type="text" name='pinCode' placeholder='PINCODE' value={data.pinCode} onChange={handleInput} />
             </div>
             <div>
-                <input style={{marginRight:"3em"}} className='inp' type="text" name='address' placeholder='ADDRESS' value={data.address} onChange = {handelInput} />
-                <input className='inp' type="text" name='optional' placeholder='OPTIONAL' value={data.optional} onChange = {handelInput} />
+                <input style={{ marginRight: "3em" }} className='inp' type="text" name='address' placeholder='ADDRESS' value={data.address} onChange={handleInput} />
+                <input className='inp' type="text" name='optional' placeholder='OPTIONAL' value={data.optional} onChange={handleInput} />
             </div>
             <div>
-                <input style={{marginRight:"3em"}} className='inp' type="text" name='locality' placeholder='LOCALITY' value={data.locality} onChange = {handelInput} />
-                <input className='inp' type="text" name='city' placeholder='CITY' value={data.city} onChange = {handelInput} />
+                <input style={{ marginRight: "3em" }} className='inp' type="text" name='locality' placeholder='LOCALITY' value={data.locality} onChange={handleInput} />
+                <input className='inp' type="text" name='city' placeholder='CITY' value={data.city} onChange={handleInput} />
             </div>
-            <div className='state'>
-            <select className='inp' onChange = {handelInput} name='state'>
+            <select className='inp' onChange={handleInput} name='state'>
                 <option value="">STATE</option>
                 <option value="Bihar">Bihar</option>
                 <option value="Mumbai">Maharashtra</option>
@@ -93,76 +106,26 @@ const Register = () => {
                 <option value="Madhya Pradesh">Madhya Pradesh</option>
                 <option value="Jammu Kashmir">Jammu Kashmir</option>
             </select>
-            <div className='reg'>
-                <p>REGION</p>
-                <p>India</p>
-            </div>
-            </div>
-            <p style={{fontSize:"8px"}}>PREFIX</p>
-            <div style={{display:"flex"}}>
-                <p style={{marginRight:"4em",marginTop:"4px", fontSize:'12px'}}>+91</p>
-                <input className='tel'  type="text" name = 'phoneNumber' placeholder='TELIPHONE' value={data.phoneNumber} onChange = {handelInput} />
+
+            <p style={{ fontSize: "8px" }}>PREFIX</p>
+            <div style={{ display: "flex" }}>
+                <p style={{ marginRight: "4em", marginTop: "4px", fontSize: '12px' }}>+91</p>
+                <input className='tel' type="text" name='phoneNumber' placeholder='TELEPHONE' value={data.phoneNumber} onChange={handleInput} />
             </div>
             <div>
                 <div>
-                    <input className='check' type="checkbox" name="news" checked={data.news} onChange = {handelInput} />
-                    <label className='lab' htmlFor="">I WISH TO RECIEVE ZARA NEWS ON MY E-MAIL</label>
+                    <input style={{ marginTop: "4px" }} type="checkbox" name="news" />
+                    <label style={{ fontSize: "12px" }}>I WISH TO RECIVE ZARA NEWS ON MY E-MAIL</label>
                 </div>
                 <div>
-                    <input className='check1' type="checkbox" name="termCondition" checked={data.termCondition} onChange = {handelInput} />
-                    <label className='lab' htmlFor="">I ACCEPT THE <u> PRIVACY STATEMENT</u></label>
+                    <input type="checkbox" name="termCondition" />
+                    <label style={{ fontSize: "12px" }}>I ACCEPT THE PRIVACY STATEMENT</label>
                 </div>
             </div>
-            <button className='button' onClick={handelCreateAcc}>CREATE ACCOUNT</button>
+            <button onClick={() => dispatch(authRegister(data))}>
+                CREATE ACCOUNT
+            </button>
         </div>
-        <div className=' bottom'>
-        <div>
-            <h3>HELP</h3>
-            <p>SHOP AT ZARA.COM</p>
-            <p>PRODUCT</p>
-            <p>GIFT CARD</p>
-            <p>PAYMENT</p>
-            <p>SHIPPING</p>
-            <p>EXCHANGES AND RETURNS</p>
-            <p>SHOPS AND COMPANY</p>
-            <p>CLOTHES COLLECTION PROGRAMME</p>
-            <p>MY ACCOUNT</p>
-        </div>
-        <div>
-            <h3>FOLLOW US</h3>
-            <p>NEWSLETTER</p>
-            <p>INSTAGRAM</p>
-            <p>FACEBOOK</p>
-            <p>TWITTER</p>
-            <p>PINTREST</p>
-            <p>YOUTUBE</p>
-        </div>
-        <div>
-            <h3>COMPANY</h3>
-            <p>ABOUT US</p>
-            <p>JOIN LIFE</p>
-            <p>OFFICES</p>
-            <p>STORIES</p>
-            <p>WORK WITH US</p>
-        </div>
-        <div>
-            <h3>POLICIES</h3>
-            <p>PRIVACY POLICY</p>
-            <p>PURCHASING CONDITIONS</p>
-            <p>GIFT CARD CONDITIONS</p>
-            <p>COOKIES SETTINGS</p>
-        </div>
-    </div>
-    <div className='line'>
-        <p>INDIA</p>
-        <p>ENGLISH  © ALL RIGHTS RESERVED</p>
-    </div>
-    <div className='bot2'>
-        <p>NAME AND ADDRESS OF THE MANUFACTURER:</p>
-        <p>INDUSTRIA DE DISEÑO TEXTIL, S.A. (INDITEX, S.A.)</p>
-        <p>AVENIDA DE LA DIPUTACIÓN, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUÑA), SPAIN</p>
-    </div>
-    </>
     )
 }
 
--- src/Components/loginLogout/UserLoginComponent.jsx
+++ src/Components/loginLogout/UserLoginComponent.jsx
@@ -1,122 +1,118 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./login.css"
+import { useDispatch, useSelector } from 'react-redux';
+import { useToast } from '@chakra-ui/react';
+import { authLogin } from '../../Redux/auth/actions';
 
 
 const UserLoginComponent = () => {
-    const [userData, setUserData] = useState(null)
-    const [text, setText] = useState({
-        email:"",
-        password:""
-    })
-    const handelInput = (e)=>{
-        setText({...text, [e.target.name]:e.target.value})
+
+    const [loginData, setLoginData] = React.useState({ email: '', password: '' });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
     }
 
-    const handedlApi = ()=>{
-        axios.get("http://localhost:8080/UserINFO").then(res=>{
-            console.log(res.data);
-            setUserData(res.data)
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
-    const navigate = useNavigate()
-    
+    const dispatch = useDispatch();
+    const toast = useToast();
+    const navigate = useNavigate();
 
-    // console.log(text.email && text.password);
-    useEffect(()=>{
-        handedlApi()
-    },[])
-    
+    const { userLogin: { loading, error, message }, data: { isAuthenticated, token, user } } = useSelector(state => state.auth);
 
-    const handelCreate = ()=>{
-        navigate("/register")
-    }
-    const handelLogin = ()=>{
-        handedlApi()
-        userData && userData.forEach((e,i)=>{
+    useEffect(() => {
+        if (isAuthenticated) {
+            toast({
+                title: `Welcome ${user.name}`,
+                description: "Your account is created",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+            });
+            let time = setTimeout(() => {
+                navigate("/");
+            }, 3000);
+            return () => clearTimeout(time);
+        }
+        if (error) {
+            toast({
+                title: message,
+                description: 'Please try again',
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            });
+        }
+    }, [isAuthenticated, error]);
+    return (
+        <>
+            <div className='main'>
 
-            if(e.email === text.email && e.password === text.password){
-                alert("Login Successfully")
-                navigate("/")
-            }else if(e.email === text.email && e.password !== text.password){
-                alert("Please Enter Correct Password")
-            }else if(e.email !== text.email && e.password === text.password){
-                alert("Please Enter Correct Email Id")
-            }else{
-                alert("Please Enter Correct Email and Password!")
-            }
-        })
-    }
-  return (
-    <>
-    <div className='main'>
-        
-        <div className='block1'>
-        <div className='h1'>LOG IN</div>
-        <input type="text" name='email' placeholder='E-MAIL' value={text.email} onChange = {handelInput}/><br/>
-        <input type="text" name='password' placeholder='PASSWORD' value={text.password} onChange = {handelInput}/><br/>
-        <p>HAVE YOU FORGOTTEN YOUR PASSWORD?</p><br/><br/>
-        <button onClick={handelLogin}>LOG IN</button>    
-        </div> 
-        <div className='block2'>
-            <h1 className='h2'>REGISTER</h1><br/>
-            <h4>IF YOU STILL DON'T HAVE A <span style={{fontWeight:"bolder"}}>ZARA.COM</span>  ACCOUNT, USE THIS OPTION TO ACCESS THE REGISTRATION FORM.</h4><br/>
-            <h4>BY GIVING US YOUR DETAILS, PURCHASING IN <span style={{fontWeight:"bolder"}}>ZARA.COM</span> WILL BE FASTER AND AN ENJOYABLE EXPERIENCE.</h4><br/><br/>
-            <button onClick={handelCreate}>CREATE ACCOUNT</button>
-        </div>
-    </div>
-    <div className=' bottom'>
-        <div>
-            <h3>HELP</h3>
-            <p>SHOP AT ZARA.COM</p>
-            <p>PRODUCT</p>
-            <p>GIFT CARD</p>
-            <p>PAYMENT</p>
-            <p>SHIPPING</p>
-            <p>EXCHANGES AND RETURNS</p>
-            <p>SHOPS AND COMPANY</p>
-            <p>CLOTHES COLLECTION PROGRAMME</p>
-            <p>MY ACCOUNT</p>
-        </div>
-        <div>
-            <h3>FOLLOW US</h3>
-            <p>NEWSLETTER</p>
-            <p>INSTAGRAM</p>
-            <p>FACEBOOK</p>
-            <p>TWITTER</p>
-            <p>PINTREST</p>
-            <p>YOUTUBE</p>
-        </div>
-        <div>
-            <h3>COMPANY</h3>
-            <p>ABOUT US</p>
-            <p>JOIN LIFE</p>
-            <p>OFFICES</p>
-            <p>STORIES</p>
-            <p>WORK WITH US</p>
-        </div>
-        <div>
-            <h3>POLICIES</h3>
-            <p>PRIVACY POLICY</p>
-            <p>PURCHASING CONDITIONS</p>
-            <p>GIFT CARD CONDITIONS</p>
-            <p>COOKIES SETTINGS</p>
-        </div>
-    </div>
-    <div className='line'>
-        <p>INDIA</p>
-        <p>ENGLISH  © ALL RIGHTS RESERVED</p>
-    </div>
-    <div className='bot2'>
-        <p>NAME AND ADDRESS OF THE MANUFACTURER:</p>
-        <p>INDUSTRIA DE DISEÑO TEXTIL, S.A. (INDITEX, S.A.)</p>
-        <p>AVENIDA DE LA DIPUTACIÓN, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUÑA), SPAIN</p>
-    </div>
-    </>
-  )
+                <div className='block1'>
+                    <div className='h1'>LOG IN</div>
+                    <input type="text" name='email' placeholder='E-MAIL' value={loginData.email} onChange={handleChange} /><br />
+                    <input type="text" name='password' placeholder='PASSWORD' value={loginData.password} onChange={handleChange} /><br />
+                    <p>HAVE YOU FORGOTTEN YOUR PASSWORD?</p><br /><br />
+                    <button onClick={() => dispatch(authLogin(loginData))}>LOG IN</button>
+                </div>
+                <div className='block2'>
+                    <h1 className='h2'>REGISTER</h1><br />
+                    <h4>IF YOU STILL DON'T HAVE A <span style={{ fontWeight: "bolder" }}>ZARA.COM</span>  ACCOUNT, USE THIS OPTION TO ACCESS THE REGISTRATION FORM.</h4><br />
+                    <h4>BY GIVING US YOUR DETAILS, PURCHASING IN <span style={{ fontWeight: "bolder" }}>ZARA.COM</span> WILL BE FASTER AND AN ENJOYABLE EXPERIENCE.</h4><br /><br />
+                    <Link to='/register'><button>CREATE ACCOUNT</button></Link>
+                </div>
+            </div>
+            <div className='bottom'>
+                <div>
+                    <h3>HELP</h3>
+                    <p>SHOP AT ZARA.COM</p>
+                    <p>PRODUCT</p>
+                    <p>GIFT CARD</p>
+                    <p>PAYMENT</p>
+                    <p>SHIPPING</p>
+                    <p>EXCHANGES AND RETURNS</p>
+                    <p>SHOPS AND COMPANY</p>
+                    <p>CLOTHES COLLECTION PROGRAMME</p>
+                    <p>MY ACCOUNT</p>
+                </div>
+                <div>
+                    <h3>FOLLOW US</h3>
+                    <p>NEWSLETTER</p>
+                    <p>INSTAGRAM</p>
+                    <p>FACEBOOK</p>
+                    <p>TWITTER</p>
+                    <p>PINTREST</p>
+                    <p>YOUTUBE</p>
+                </div>
+                <div>
+                    <h3>COMPANY</h3>
+                    <p>ABOUT US</p>
+                    <p>JOIN LIFE</p>
+                    <p>OFFICES</p>
+                    <p>STORIES</p>
+                    <p>WORK WITH US</p>
+                </div>
+                <div>
+                    <h3>POLICIES</h3>
+                    <p>PRIVACY POLICY</p>
+                    <p>PURCHASING CONDITIONS</p>
+                    <p>GIFT CARD CONDITIONS</p>
+                    <p>COOKIES SETTINGS</p>
+                </div>
+            </div>
+            <div className='line'>
+                <p>INDIA</p>
+                <p>ENGLISH  © ALL RIGHTS RESERVED</p>
+            </div>
+            <div className='bot2'>
+                <p>NAME AND ADDRESS OF THE MANUFACTURER:</p>
+                <p>INDUSTRIA DE DISEÑO TEXTIL, S.A. (INDITEX, S.A.)</p>
+                <p>AVENIDA DE LA DIPUTACIÓN, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUÑA), SPAIN</p>
+            </div>
+        </>
+    )
 }
 
 export default UserLoginComponent
